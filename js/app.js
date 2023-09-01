d1b99cca6330e739a642ad668edf1185561f409f
ggomez1403 <geralgomez1403@gmail.com>
2023-08-31 20:55:46 -0500

    js file updated

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 const navBtn = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
 
-console.log(navBtn);
-console.log(navLinks);
-
 navBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
 });
