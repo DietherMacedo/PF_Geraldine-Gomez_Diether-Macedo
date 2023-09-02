997491beda5dbf152d706584fecaa0dcf9dd9847
ggomez1403 <geralgomez1403@gmail.com>
2023-09-02 16:48:39 -0500

    footer added and navbar optimized

--- js/app.js
+++ js/app.js
@@ -1,6 +1,21 @@
 const navBtn = document.querySelector(".nav-toggle");
 const navLinks = document.querySelector(".nav-links");
+const submitBtn = document.querySelector(".submit-btn");
+
+const navIconFunc = () => {
+  if (navBtn.innerHTML.includes("fa-bars")) {
+    navLinks.classList.add("show-links");
+    navBtn.innerHTML = `<i class="fa-solid fa-x"></i>`;
+  } else {
+    navLinks.classList.remove("show-links");
+    navBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  }
+};
 
 navBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("show-links");
+  navIconFunc();
+});
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
 });
